8272025d0af1e60e69e25c95a5125324ea808f62
Xavier Challe <xavier.challe@ledger.fr>
2025-11-27 15:56:31 +0100

    chore: remove node_modules from repo

--- actions/create-github-release/node_modules/@manypkg/tools/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from "./Tool.ts";
-export { BunTool } from "./BunTool.ts";
-export { LernaTool } from "./LernaTool.ts";
-export { NpmTool } from "./NpmTool.ts";
-export { PnpmTool } from "./PnpmTool.ts";
-export { RootTool } from "./RootTool.ts";
-export { RushTool } from "./RushTool.ts";
-export { YarnTool } from "./YarnTool.ts";
